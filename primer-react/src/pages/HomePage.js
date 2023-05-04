003b741685f0781725efc83ed203f94b59363e98
¨Franco <franco_gorena98@hotmail.com>
2023-05-04 12:14:20 -0300

    ¨homePage¨

--- primer-react/src/pages/HomePage.js
+++ primer-react/src/pages/HomePage.js
@@ -6,8 +6,26 @@ const HomePage = (props) => {
             <div className="homeimg">
                 <img src="/Img/avion.png" />
             </div>
-            
-
+            <div className="columnas">
+                <div className="bienvenidos">
+                    <h2>Bienvenidos</h2>
+                    <p>Id ea deserunt sint consectetur aliquip anim qui minim pariatur ut eu. Non nostrud non
+                        nostrud occaecat enim commodo adipisicing irure esse velit.
+                        Minim magna eu occaecat nulla. Consequat sunt sint do occaecat nisi.
+                        Tempor elit ea nisi commodo et.</p>
+                    <p>Id ea deserunt sint consectetur aliquip anim qui minim pariatur ut eu. Non nostrud non
+                        nostrud occaecat enim commodo adipisicing irure esse velit.
+                        Minim magna eu occaecat nulla. Consequat sunt sint do occaecat nisi.
+                        Tempor elit ea nisi commodo et.</p>
+                </div>
+                <div className="testimonios">
+                    <h2>Testimonios</h2>
+                    <div className="testimono">
+                        <span className="cita"> Simplemente excelente</span>
+                        <span className="autor">Juan Perez</span>
+                     </div>
+                </div>
+            </div>
         </main>
     );
 }
