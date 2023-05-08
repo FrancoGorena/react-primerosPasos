0be4617061a7ac21fa05d4b1826c00acc59bb9df
¨Franco <franco_gorena98@hotmail.com>
2023-05-08 00:32:17 -0300

    ¨contactos¨

--- primer-react/src/pages/ContactosPage.js
+++ primer-react/src/pages/ContactosPage.js
@@ -1,7 +1,44 @@
 import React from "react";
 const ContactosPage = (props) => {
     return (
-        <div>ContactosPage</div>
+        <main className="holder contacto">
+            <div>
+                <h2>Contacto rápido</h2>
+                <form action="" method="" className="formulario">
+                    <p>
+                        <label for="nombre">Nombre</label>
+                        <input type="text" name="" />
+                    </p>
+                    <p>
+                        <label for="email">Email</label>
+                        <input type="text" name="" />
+                    </p>
+                    <p>
+                        <label for="telefono">Telefono</label>
+                        <input type="number" name="" />
+                    </p>
+                    <p>
+                        <label for="mensaje">Mensaje</label>
+                        <textarea name=""></textarea>
+                    </p>
+                    <p class="acciones"><input type="submit" value="Enviar" />
+                    </p>
+                </form>
+            </div>
+            <div calss="datos">
+                <h2>Otras vias de comunicación</h2>
+                <p>También puedes contactarte con nosotros usando los siguientes
+                    medios</p>
+                    <ul>
+                        <li>Teléfono: 4273676</li>
+                        <li>Email: pepito14@gmail.com</li>
+                        <li>Facebook:</li>
+                        <li>Instagram:</li>
+                        <li>Skype:</li>
+                    </ul>
+
+            </div>
+        </main>
     );
 }
 
